598e3c5703a1708b288e21cf08312a9db04809a6
SystemXVoid <94419318+SystemXVoid@users.noreply.github.com>
2024-08-14 01:52:53 -0400

    Update MainScript.lua

--- packages/MainScript.lua
+++ packages/MainScript.lua
@@ -93,12 +93,7 @@ local platform = ({pcall(function() return inputService:GetPlatform() end)})[2]
 if identifyexecutor and identifyexecutor():lower():find('syn') then 
 	 local oldrequire = getgenv().oldrequire or require;
 	 getgenv().oldrequire = require;
-	 getgenv().require = function(module: ModuleScript?)
-		if checkcaller() then 
-			return error('syn z is trash')
-		end
-		return oldrequire(module)
-	 end 
+	 getgenv().require = getrenv().require;
 end;
 
 --[[ you dont need this anymore lol
